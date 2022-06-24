44f00620aa14a8901c0ae2dcaeceaa2450d2c9c0
hayato2022 <gaomusunxiang@gmail.com>
2022-06-24 01:53:56 +0000

    [add]log_in bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+
   devise_for :admin, skip: [:registrations, :passwords], controllers:{
     sessions: "admin/sessions"
   }
 
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: "homes#top"
 end
