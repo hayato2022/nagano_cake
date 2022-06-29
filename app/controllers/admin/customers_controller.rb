389bc0247b0194462584808397f04795ad7f1827
hayato2022 <gaomusunxiang@gmail.com>
2022-06-29 09:41:33 +0000

    [start]admin_customers

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,20 @@
 class Admin::CustomersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def customer_params
+  end
+
 end
