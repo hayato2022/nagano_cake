324d9cb0dfadceec86fa30b8f667f21ad3d19a86
hayato2022 <gaomusunxiang@gmail.com>
2022-07-04 09:29:25 +0000

    [add]public routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  
+
   # 会員側routes
     devise_for :customers, skip: [:passwords], controllers:{
       registrations: "public/registrations",
       sessions: "public/sessions"
     }
-  
-  root to: "homes#top"
-  
+
+  scope module: :public do
+    root to: "homes#top"
+  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
