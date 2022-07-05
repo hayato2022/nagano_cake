0de750dc5536d3c2ebf8278bb17ed13c07868bc3
hayato2022 <gaomusunxiang@gmail.com>
2022-07-05 08:09:15 +0000

    [add]image_size

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,12 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :orders_details, dependent: :destroy
 
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
