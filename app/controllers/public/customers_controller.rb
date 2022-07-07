f2af199422a63c3a036fcce4f70dd57382ddfad6
hayato2022 <gaomusunxiang@gmail.com>
2022-07-07 07:16:40 +0000

    [add]customer_update

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,12 +9,22 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_my_page_path
   end
 
   def unsubscribe
   end
 
   def withdrawal
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana,
+              :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 
 end
