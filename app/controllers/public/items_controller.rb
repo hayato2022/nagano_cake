1752f2dba46c4b08d7ca9be75fdaa6c3b7702a11
hayato2022 <gaomusunxiang@gmail.com>
2022-07-12 07:11:50 +0000

    [add]item/index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,15 +1,24 @@
 class Public::ItemsController < ApplicationController
 
   def index
+    # サイドバーにジャンル一覧を表示させるために、ジャンルを全取得
     @genres = Genre.all
-
+    
+    # urlにgenre_idがある場合
     if params[:genre_id]
+      
+      # genreのデータベースから一致するidを取得する
       @genre = Genre.find(params[:genre_id])
+      
+      # genreと紐づく商品を取得
       @items = @genre.items.all
+      
+      # ジャンルをクリックした場合は、genreの名前
       @item_name = @genre.name
     else
-
+      #商品をすべて取得
       @items = Item.all
+      # クリックしていない場合は「商品」一覧
       @item_name = "商品"
     end
   end
