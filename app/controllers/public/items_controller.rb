2f67c981b40664cffb3380eaff9ef1890246dfe8
hayato2022 <gaomusunxiang@gmail.com>
2022-07-12 03:11:26 +0000

    [intermediate]item_index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,9 +3,15 @@ class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
 
-    @items = Item.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.all
+      @item_name = @genre.name
+    else
 
-
+      @items = Item.all
+      @item_name = "商品"
+    end
   end
 
   def show
