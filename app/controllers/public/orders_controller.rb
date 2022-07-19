1e03e7af133b06f426eaa986094912d73b25b9bf
hayato2022 <gaomusunxiang@gmail.com>
2022-07-19 10:28:07 +0000

    [add]order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,16 +8,26 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @cart_items = current_customer.cart_items.all
+    @postage = 800 #送料
+    @total =0
+    @amount_billed = 0
+
     @order = Order.new(order_params)
     # 選択された住所が自身の住所の場合。ラジオボタンの:select_addressが0
-    if params([:order][:select_address]) == "0"
+    if params[:order][:payment_method] == "credit_card"
+      @payment_method = "クレジットカード"
+    elsif params[:order][:payment_method] == "transfer"
+      @payment_method = "銀行振込"
+    end
+
+    if params[:order][:select_address] == '0'
       # ログインしている会員の郵便番号、住所、名前を取得
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.last_name + current_customer.first_name
 
     # 選択された住所が「登録済み住所から選択」の場合。ラジオボタンの:select_addressが1
-    elsif params([:order][:select_address]) =="1"
+    elsif params[:order][:select_address] == '1'
       # 送られた情報をもとに配送先モデルから検索。配送先(Address)モデルから送られてきた ID をもとに取得
       @address = Address.find(params[:order][:address_id])
       # @order の中に配送先(Address)モデルから取得した情報を格納
@@ -26,7 +36,7 @@ class Public::OrdersController < ApplicationController
       @order.name = @address.name
 
       # 選択された住所が新しいお届け先の場合。ラジオボタン:select_addressの値が２
-    elsif params([:order][:select_address] == "2")
+    elsif params[:order][:select_address] == '2'
       @order.postal_code = @order.postal_code
       @order.address = @order.address
       @order.name = @order.name
