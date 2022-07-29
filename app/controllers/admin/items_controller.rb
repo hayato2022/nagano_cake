39c5daf96211ec3ceb2f8c9eccab19c0abbde456
hayato2022 <gaomusunxiang@gmail.com>
2022-07-29 02:10:26 +0000

    [add]admin/item/index layouts

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,7 +11,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def index
-    @items = Item.all
+    @items = Item.page(params[:page]).per(10)
 
   end
 
@@ -30,7 +30,7 @@ class Admin::ItemsController < ApplicationController
     redirect_to admin_item_path(@item.id)
   end
 
-  
+
 
   private
 
